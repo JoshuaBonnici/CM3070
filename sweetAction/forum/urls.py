81eda49c6eb4c491b3b2fa5a0f8d6001691228b0
Joshua Bonnici <j.n.bonnici@gmail.com>
2024-08-25 19:04:40 -0400

    Updated homepage and css

--- sweetAction/forum/urls.py
+++ sweetAction/forum/urls.py
@@ -9,11 +9,13 @@ from . import views
 # setup the url paths for the endpoints and index
 urlpatterns = [
     path('', views.index, name='index'),
-]
+    path('general.html', views.general, name='general'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('brunch.html', views.brunch, name='brunch'),
+    path('suggestionbox.html', views.suggestionbox, name='suggestionbox'),
+    path('placeridiculous.html', views.placeridiculous, name='placeridiculous'),]
 
-
-    # path('login/', views.user_login, name='login'),
-    # path('logout/', views.user_logout, name='logout'),
     # # example url path for a page that can only be viewed via login.
     # # Having the below sends the user to the login page if they try to click the link without being logged in
     # path('logout/', login_required(login_url='../login/')(views.user_logout), name='logout'),
--- sweetAction/forum/views.py
+++ sweetAction/forum/views.py
@@ -5,3 +5,24 @@ from django.shortcuts import render
 # define the index page view
 def index(request):
     return render(request, 'forum/index.html')
+
+def general(request):
+    return render(request, 'forum/general.html')
+
+def user_login(request):
+    return render(request, 'forum/login.html')
+
+def user_logout(request):
+    return render(request, 'forum/logout.html')
+
+
+
+def brunch(request):
+    return render(request, 'forum/brunch.html')
+
+
+def suggestionbox(request):
+    return render(request, 'forum/suggestionbox.html')
+
+def placeridiculous(request):
+    return render(request, 'forum/placeridiculous.html')
